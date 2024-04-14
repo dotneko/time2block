2effcef85ad698a2e8c3e585747346b08cc8860d
neko <neko@dotneko>
2024-04-14 10:28:03 +0800

    Minor fixes

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ Flags:
 	-d, --detail    Show estimated date/time and details
 	-r, --raw       Show time left in seconds in raw format
 	-l, --local     Show estimated local date/time until target block reached
+
 	-t, --time      Show estimated UTC date/time until target block reached
 
 	-h, --help      Show this message
@@ -109,24 +110,24 @@ func main() {
 	n /= 24
 	if displayFmt == DETAIL {
 		currentTime := time.Now()
-		fmt.Printf("Blocks remaining until block %d: %d, Block time: %v\n", targetBlock, blocksLeft, blockTime)
+		fmt.Printf("Blocks remaining     : %d, Block time: %v\n", blocksLeft, blockTime)
 		fmt.Printf("Estimated time left  : %dd %02dh %02dm %02ds\n", n, hours, mins, secs)
 		fmt.Printf("UTC Time now         : %v\n", currentTime.UTC())
-		fmt.Printf("Estimated target time: %v\n", currentTime.Add(time.Duration(seconds)*time.Second).UTC())
-		fmt.Printf("Local Time now       : %v\n", currentTime.Local())
-		fmt.Printf("Estimated target time: %v\n", currentTime.Add(time.Duration(seconds)*time.Second).Local())
+		fmt.Printf("Estimated local time : %v\n", currentTime.Add(time.Duration(seconds)*time.Second).UTC())
+		if currentTime.UTC() != currentTime.Local() {
+			fmt.Printf("Local Time now       : %v\n", currentTime.Local())
+			fmt.Printf("Estimated target time: %v\n", currentTime.Add(time.Duration(seconds)*time.Second).Local())
+		}
 	} else if displayFmt == LOCAL {
 		currentTime := time.Now()
-		fmt.Printf("Estimated target time: %v\n", currentTime.Add(time.Duration(seconds)*time.Second).Local())
+		fmt.Printf("Estimated local time: %v\n", currentTime.Add(time.Duration(seconds)*time.Second).Local())
 	} else if displayFmt == RAW {
 		fmt.Printf("%v", seconds)
 	} else if displayFmt == TIME {
 		currentTime := time.Now()
-		fmt.Printf("Estimated target time: %v\n", currentTime.Add(time.Duration(seconds)*time.Second).UTC())
+		fmt.Printf("Estimated target time : %v\n", currentTime.Add(time.Duration(seconds)*time.Second).UTC())
 	} else {
-		// displayFmt == FULL
-		fmt.Printf("Blocks remaining     : %d (Target %d, avg block time: %v)\n", blocksLeft, targetBlock, blockTime)
-		fmt.Printf("Estimated target time: %dd %02dh %02dm %02ds (%vs) \n", n, hours, mins, secs, seconds)
+		fmt.Printf("Time remaining: %dd %02dh %02dm %02ds (%vs) \n", n, hours, mins, secs, seconds)
 	}
 
 }
